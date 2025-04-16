a72da87947c8e86a24e67dd22c22882cd2b5faab
SemHones <s.hones@outlook.com>
2025-04-16 15:15:16 +0200

    build

--- adaptTerrariaWiki/terraria_api/terraria_api/obj/Debug/net8.0/terraria_api.AssemblyInfo.cs
+++ adaptTerrariaWiki/terraria_api/terraria_api/obj/Debug/net8.0/terraria_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("terraria_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9511eceb56c538bc489f2493e96fd9882dc29b0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f52bf5b9c9581075ffb2feddd3f68377a3a0d0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("terraria_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("terraria_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
