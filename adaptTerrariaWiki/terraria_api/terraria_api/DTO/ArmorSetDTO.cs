e39fab6b9b1d8316bb2bf86a283ab907b8a5845d
SemHones <s.hones@outlook.com>
2025-04-17 15:49:58 +0200

    Create ArmorSetDTO.cs

---
+++ adaptTerrariaWiki/terraria_api/terraria_api/DTO/ArmorSetDTO.cs
@@ -0,0 +1,16 @@
+﻿using terraria_api.Models;
+
+namespace terraria_api.DTO
+{
+    public class ArmorSetDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Href { get; set; }
+        public string SetBonus { get; set; }
+        public int HeadId { get; set; }
+        public int BodyId { get; set; }
+        public int LegsId { get; set; }
+    }
+}
