15fdbdf27c138925c4b390c1b24cab6b7d31ba6c
SemHones <s.hones@outlook.com>
2025-04-18 15:38:56 +0200

    generator for armor json files is done

--- adaptTerrariaWiki/webscraper/main.py
+++ adaptTerrariaWiki/webscraper/main.py
@@ -6,8 +6,8 @@ url1 = "https://terraria.fandom.com/wiki/Ranged_weapons"
 url2 = "https://terraria.fandom.com/wiki/Melee_weapons"
 def main():
     # sd.ScrapeAllTablesFromTerSite(url1, "Ranged accessory")
-    sd.ScrapeAllTablesFromTerSite(url2, "Melee armor")
+    # sd.ScrapeAllTablesFromTerSite(url2, "Melee armor")
     # sd.ScrapeAllTablesFromTerSite(url1, "Ranged armor")
-    # sd.ScrapeAllTablesFromTerSite(url1, "Ranged weapon")
+    sd.ScrapeAllTablesFromTerSite(url1, "Ranged weapon")
 
 main()
--- adaptTerrariaWiki/webscraper/scrapeData.py
+++ adaptTerrariaWiki/webscraper/scrapeData.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import base64
 import requests
+import re
 
 class ScrapeData:
     def ScrapeAllTablesFromTerSite(self, url, tableTitle):
@@ -38,22 +39,33 @@ class ScrapeData:
             "tableTitle": tableTitle,
             "contents": []
         }
+        # Indexes are purely for debugging purposes
+        armorIndex = 0
+        weaponIndex = 0
+        accessoryIndex = 0
         for row in table.find_all('tr')[1:]:  
+            
             row_data = {}
             
             link_tag = row.find('a')
             cells = row.find_all(['th', 'td'])
             if(link_tag):
+                print("data")
+                print(data)
+
                 if "armor" in tableTitle:
-                    data = self.returnJsonOfArmorTable(link_tag, row_data, cells, data)
+                    data = self.returnJsonOfArmorTable(link_tag, row_data, cells, data, armorIndex)
+                    armorIndex += 1
                 elif "weapon" in tableTitle:
-                    data = self.returnJsonOfWeaponTable(link_tag, row_data, cells, data)
+                    data = self.returnJsonOfWeaponTable(link_tag, row_data, cells, data, weaponIndex)
+                    weaponIndex += 1
                 elif "accessory" in tableTitle:
-                    data = self.returnJsonOfAccessoryTable(link_tag, row_data, cells, data)
+                    data = self.returnJsonOfAccessoryTable(link_tag, row_data, cells, data, accessoryIndex)
+                    accessoryIndex += 1
         return data
 
     # also loads buffs and debuffs 
-    def returnJsonOfAccessoryTable(self, link_tag, row_data, cells, data):
+    def returnJsonOfAccessoryTable(self, link_tag, row_data, cells, data, accessoryIndex):
         if cells is None:
             return data
         
@@ -63,6 +75,8 @@ class ScrapeData:
 
         if link_tag:
             first_image = link_tag.find('img')
+            print("first_image")
+            print(first_image)
             if first_image:
                 row_data['name'] = first_image['alt']
                 if "http" in first_image['src']:
@@ -76,47 +90,48 @@ class ScrapeData:
         data["contents"].append(row_data)
         return data
     
-    def returnJsonOfArmorTable(self, link_tag, row_data, cells, data):
+    def returnJsonOfArmorTable(self, link_tag, row_data, cells, data, armorIndex):
         if link_tag:
             first_image = link_tag.find('img')
             if first_image:
                 # check to make sure only armors are being scraped
-                if not "armor" in first_image['alt']:
+                if not "armor" in first_image['data-src']:
                     print("Not an armor")
                     return data
-                print("Armor found")
-                row_data['name'] = first_image['alt']
+                # make the print work
+                print("Armor found " + str(armorIndex))
+
                 if "http" in first_image['src']:
                     row_data['image'] = self.convertUrlTobase64(first_image['src'])
                 elif "http" in first_image['data-src']:
                     row_data['image'] = self.convertUrlTobase64(first_image['data-src'])
             row_data['href'] = link_tag['href']
-        if("Head:" in cells[2].get_text(strip=True)):
+            
+        row_data['name'] = cells[1].get_text(strip=True)
+        row_data['head'] = cells[2].get_text(strip=True)
+        row_data['chest'] = cells[3].get_text(strip=True)
+        row_data['legs'] = cells[4].get_text(strip=True)
+        row_data['bonus'] = self.extract_bonus_text(cells[6])
+        if("Head:" in cells[7].get_text(strip=True)):
             row_data['obtained_by'] = "Crafted"
         else:
-            row_data['obtained_by'] = self.filterString(cells[2].get_text())
-            
-        row_data['head'] = cells[3].get_text(strip=True)
-        row_data['chest'] = cells[4].get_text(strip=True)
-        row_data['legs'] = cells[5].get_text(strip=True)
-        row_data['summary'] = cells[6].get_text(strip=True)
-        row_data['bonus'] = self.filterString(cells[7].get_text())
+            row_data['obtained_by'] = self.extract_obtained_by(cells[7])
 
         # Append the row data to the list
         data["contents"].append(row_data)
         return data
             
-    def returnJsonOfWeaponTable(self, link_tag, row_data, cells, data):
+    def returnJsonOfWeaponTable(self, link_tag, row_data, cells, data, weaponIndex):
         
         if link_tag:
             first_image = link_tag.find('img')
             if first_image:
                 # check to make sure only weapons are being scraped
                 if "armor" in first_image['alt']:
+                    print("Not an weapon")
                     return data
+                # print("Weapon found " + str(weaponIndex))
                 row_data['name'] = first_image['alt']
-                # print(first_image['src'])
-                # print(first_image['data-src'])
                 if "http" in first_image['src']:
                     row_data['image'] = self.convertUrlTobase64(first_image['src'])
                 elif "http" in first_image['data-src']:
@@ -126,7 +141,7 @@ class ScrapeData:
         if("Crafted" in cells[4].get_text(strip=True)):
             row_data['obtained_by'] = "Crafted"
         else:
-            row_data['obtained_by'] = self.filterString(cells[4].get_text())
+            row_data['obtained_by'] = self.extract_obtained_by(cells[4])
         row_data['notes'] = self.filterString(cells[5].get_text())
 
         # Append the row data to the list
@@ -157,3 +172,35 @@ class ScrapeData:
             print("Error:", e)
             return url
         
+    def extract_bonus_text(self, cell):
+        soup = BeautifulSoup(str(cell), 'html.parser')
+        bonuses = []
+
+        for li in soup.find_all('li'):
+            span = li.find('span')
+            
+            # Als er geen <span> is, of de span heeft GEEN title met "PC", skip
+            if span and "PC" not in span.get("title", ""):
+                continue
+
+            # Tekst uit de li zelf (zonder span-elementen)
+            # .get_text() haalt alles op (ook nested), maar strip() maakt het netter
+            li_text = li.get_text(strip=True)
+            bonuses.append(li_text)
+
+        return "\n".join(bonuses)
+    
+    def extract_obtained_by(self, html):
+        soup = BeautifulSoup(str(html), 'html.parser')
+        result = []
+
+        td = soup.find('td', class_='small')
+        if not td:
+            return ""
+
+        li_elements = td.find_all('li')
+        for li in li_elements:
+            line = ''.join(part.strip() for part in li.stripped_strings)
+            result.append(line)
+
+        return "\n".join(result)
